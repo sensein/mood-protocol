1f787d5ddef44b9050e16b855a4c592a730a0bd2
Sooyoung Ahn <sooyoungka@gmail.com>
2022-11-15 17:36:00 -0500

    updated config js file

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/sooyounga/mood-protocol/master/moodprotocol/mood_protocol_schema',
+  githubSrc: 'https://raw.githubusercontent.com/sooyounga/mood-protocol/main/moodprotocol/mood_protocol_schema',
   banner: 'Mood Disorder Protocol',
   startButton: 'Join',
-  assetsPublicPath: '/demo-protocol/',
+  assetsPublicPath: '/mood-protocol/',
   backendServer: null
 };
