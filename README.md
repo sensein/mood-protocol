3027192e9111614f3a6ce872fb9daa60dcfbe4de
sooyounga <sooyoungka@gmail.com>
2022-11-17 20:44:46 +0000

    Deploying to gh-pages from  @ 89c7c0e510a824d1233b071f03d614324d080f12 🚀

--- README.md
+++
@@ -1,3 +0,0 @@
-# This repository contains a demonstration protocol for ReproSchema
-
-Protocol for mood disorders
--- ui-changes/src/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/sooyounga/mood-protocol/master/moodprotocol/mood_protocol_schema',
-  banner: 'Mood Disorder Protocol',
-  startButton: 'Join',
-  assetsPublicPath: '/demo-protocol/',
-  backendServer: null
-};
